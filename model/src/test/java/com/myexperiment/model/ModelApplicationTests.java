ac041b3e41b5883c20d38f9f96654643b3259e45
farhan.syakir <farhan0syakir@gmail.com>
2023-02-17 17:10:38 +0200

    run artifactory local

---
+++ model/src/test/java/com/myexperiment/model/ModelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myexperiment.model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
