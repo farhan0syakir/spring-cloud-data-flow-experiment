43cf668ceef494f87af6660953ed6ca533ab7289
farhan.syakir <farhan0syakir@gmail.com>
2023-03-07 15:25:08 +0200

    add demo source

---
+++ MyDemoSource/src/main/java/com/myexample/mydemosource/MyDemoSourceApplication.java
@@ -0,0 +1,30 @@
+package com.myexample.mydemosource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Supplier;
+
+@EnableScheduling
+@SpringBootApplication
+public class MyDemoSourceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyDemoSourceApplication.class, args);
+    }
+
+    @Bean
+    public Supplier<String> graalSupplier() {
+        final String[] splitWoodchuck = "How much wood could a woodchuck chuck if a woodchuck could chuck wood?"
+                .split(" ");
+        final AtomicInteger wordsIndex = new AtomicInteger(0);
+        return () -> {
+            int wordIndex = wordsIndex.getAndAccumulate(splitWoodchuck.length,
+                    (curIndex, numWords) -> curIndex < numWords - 1 ? curIndex + 1 : 0);
+            return splitWoodchuck[wordIndex];
+        };
+    }
+
+}
---
+++ MyDemoSource/src/test/java/com/myexample/mydemosource/MyDemoSourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myexample.mydemosource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDemoSourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
