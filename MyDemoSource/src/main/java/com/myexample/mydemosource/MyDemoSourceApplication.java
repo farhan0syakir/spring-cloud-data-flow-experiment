38fb70a1b756d0f90a6ac6d1b25bcfaeaab70cfd
farhan.syakir <farhan0syakir@gmail.com>
2023-03-14 15:58:44 +0200

    add config

--- MyDemoSource/src/main/java/com/myexample/mydemosource/MyDemoSourceApplication.java
+++ MyDemoSource/src/main/java/com/myexample/mydemosource/MyDemoSourceApplication.java
@@ -3,12 +3,10 @@ package com.myexample.mydemosource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Supplier;
 
-@EnableScheduling
 @SpringBootApplication
 public class MyDemoSourceApplication {
     public static void main(String[] args) {
