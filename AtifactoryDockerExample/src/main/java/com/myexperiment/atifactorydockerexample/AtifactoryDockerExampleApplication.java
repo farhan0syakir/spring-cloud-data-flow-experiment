366b6a8bd8e170bbf2416810f5dfe4908e6ddeaa
farhan.syakir <farhan0syakir@gmail.com>
2023-03-17 11:23:19 +0200

    add new project

---
+++ AtifactoryDockerExample/src/main/java/com/myexperiment/atifactorydockerexample/AtifactoryDockerExampleApplication.java
@@ -0,0 +1,13 @@
+package com.myexperiment.atifactorydockerexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtifactoryDockerExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtifactoryDockerExampleApplication.class, args);
+    }
+
+}
---
+++ AtifactoryDockerExample/src/test/java/com/myexperiment/atifactorydockerexample/AtifactoryDockerExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myexperiment.atifactorydockerexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtifactoryDockerExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
